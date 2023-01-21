ddc5a00f7c0fbe7def114ff3deb301b49d6d1872
Azizbek Asadov <azizbek1998@icloud.com>
2023-01-21 14:12:57 +0500

    Initial Commit

---
+++ MV_SwiftUI/MV_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MV_SwiftUIApp.swift
+//  MV_SwiftUI
+//
+//  Created by Azizbek Asadov on 21/01/23.
+//
+
+import SwiftUI
+
+@main
+struct MV_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MV_SwiftUIUITests/MV_SwiftUIUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  MV_SwiftUIUITestsLaunchTests.swift
+//  MV_SwiftUIUITests
+//
+//  Created by Azizbek Asadov on 21/01/23.
+//
+
+import XCTest
+
+final class MV_SwiftUIUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
